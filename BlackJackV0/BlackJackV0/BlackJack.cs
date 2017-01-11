7dfa7e96589eae9df591941d6a1c07ebbfc39c86
reapercage <h4026@student.jamk.fi>
2017-01-11 14:58:20 +0200

    Game mustajaska is now in it's own method. Lotto file added.

--- BlackJackV0/BlackJackV0/BlackJack.cs
+++ BlackJackV0/BlackJackV0/BlackJack.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Veikkaus;
 /// <summary>
 /// A simple example of Blackjack card game
 /// </summary>
@@ -8,41 +9,50 @@ namespace JAMK.IT
   {
     static void Main()
     {
-      int myNumber = 17;
-      int theirNumber = 1;
-      string inputNumber;
-      bool exitTruth = false;
-      System.Console.WriteLine("*** BlackJack! ***");
 
-            while (exitTruth == false) {
-      System.Console.Write("Can you beat my number? Enter any number between 1-21: ");
-            //reading and converting 
-            //checks if player wants to exit
-            inputNumber = System.Console.ReadLine();
-            if(inputNumber == "x" || inputNumber == "X" || inputNumber == "exit" || inputNumber == "EXIT")
+            veikkaus.Lotto.DrawLotto();
+
+        }
+        
+        static void mustajaska()
+        {
+
+            int myNumber = 17;
+            int theirNumber = 1;
+            string inputNumber;
+            bool exitTruth = false;
+            System.Console.WriteLine("*** BlackJack! ***");
+
+            while (exitTruth == false)
             {
-                exitTruth = true;
-            }
-            else
-            {
-                theirNumber = System.Convert.ToInt32(inputNumber);
-                if (theirNumber < 1 || theirNumber > 21)
+                System.Console.Write("Can you beat my number? Enter any number between 1-21: ");
+                //reading and converting 
+                //checks if player wants to exit
+                inputNumber = System.Console.ReadLine();
+                if (inputNumber == "x" || inputNumber == "X" || inputNumber == "exit" || inputNumber == "EXIT")
                 {
-                    Console.WriteLine("The given number is out of limits, try again.");
+                    exitTruth = true;
                 }
                 else
                 {
-                    //comparing
-                    if (theirNumber >= myNumber && theirNumber <= 21)
+                    theirNumber = System.Convert.ToInt32(inputNumber);
+                    if (theirNumber < 1 || theirNumber > 21)
                     {
-                        System.Console.WriteLine("You win.");
+                        Console.WriteLine("The given number is out of limits, try again.");
                     }
                     else
                     {
-                        System.Console.WriteLine("You lose.");
+                        //comparing
+                        if (theirNumber >= myNumber && theirNumber <= 21)
+                        {
+                            System.Console.WriteLine("You win.");
+                        }
+                        else
+                        {
+                            System.Console.WriteLine("You lose.");
+                        }
                     }
                 }
-            }
                 //old code
                 //theirNumber = System.Convert.ToInt32(System.Console.ReadLine());
                 //comparing that given umber is valid
---
+++ BlackJackV0/BlackJackV0/LottoFI.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace veikkaus
+{
+    public class Lotto
+    {
+        public static void DrawLotto()
+        {
+            Console.WriteLine("Arvotaan viikon oikeat lottorivit");
+        }
+    }
+}
