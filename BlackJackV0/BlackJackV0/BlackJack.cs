0ee8f36850d210a1e4e3df786f8d838842684744
reapercage <h4026@student.jamk.fi>
2017-01-11 14:45:36 +0200

    Game can no be played multiple times.

--- BlackJackV0/BlackJackV0/BlackJack.cs
+++ BlackJackV0/BlackJackV0/BlackJack.cs
@@ -9,26 +9,48 @@ namespace JAMK.IT
     static void Main()
     {
       int myNumber = 17;
-      int theirNumber;
+      int theirNumber = 1;
+      string inputNumber;
+      bool exitTruth = false;
       System.Console.WriteLine("*** BlackJack! ***");
+
+            while (exitTruth == false) {
       System.Console.Write("Can you beat my number? Enter any number between 1-21: ");
-      //reading and converting 
-      theirNumber = System.Convert.ToInt32(System.Console.ReadLine());
-      //comparing that given umber is valid
-      if (theirNumber < 1 || theirNumber > 21)
-      {
-        Console.WriteLine("The given number is out of limits, try again.");
-      }
-      else {
-        //comparing
-        if (theirNumber >= myNumber && theirNumber <= 21)
-        {
-          System.Console.WriteLine("You win.");
+            //reading and converting 
+            //checks if player wants to exit
+            inputNumber = System.Console.ReadLine();
+            if(inputNumber == "x" || inputNumber == "X" || inputNumber == "exit" || inputNumber == "EXIT")
+            {
+                exitTruth = true;
+            }
+            else
+            {
+                theirNumber = System.Convert.ToInt32(inputNumber);
+                if (theirNumber < 1 || theirNumber > 21)
+                {
+                    Console.WriteLine("The given number is out of limits, try again.");
+                }
+                else
+                {
+                    //comparing
+                    if (theirNumber >= myNumber && theirNumber <= 21)
+                    {
+                        System.Console.WriteLine("You win.");
+                    }
+                    else
+                    {
+                        System.Console.WriteLine("You lose.");
+                    }
+                }
+            }
+                //old code
+                //theirNumber = System.Convert.ToInt32(System.Console.ReadLine());
+                //comparing that given umber is valid
+
+            } //while
+            System.Console.WriteLine("Exit");
+            Console.ReadLine();
+
         }
-        else {
-          System.Console.WriteLine("You lose.");
-        }
-      }
-    }
   }
 }
